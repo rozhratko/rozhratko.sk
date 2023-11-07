1ba2aec94229df7aca51375b5b27c055f9e9c4e2
Martin Jakubik <martindzejky@gmail.com>
2023-11-07 13:13:17 +0100

    center the title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
-
 <template>
   <main>
     <h1>Rozhrátko</h1>
   </main>
 </template>
+
+<style scoped>
+main {
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+}
+</style>
