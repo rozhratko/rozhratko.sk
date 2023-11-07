9cbd93ecde208cc8a2ba9ee8c54c4bb8b9442c54
Martin Jakubik <martindzejky@gmail.com>
2023-11-07 13:15:53 +0100

    basic styling and font

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,12 @@ main {
   min-height: 100vh;
   display: grid;
   place-items: center;
+  font-family: 'Sriracha', sans-serif;
+  background: radial-gradient(#fff, #fdfdfd);
+}
+
+h1 {
+  font-size: 5rem;
+  color: #222;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import 'normalize.css'
+import '@fontsource/sriracha'
 
 import { createApp } from 'vue'
 import App from './App.vue'
