6772cd34869def365c8fcda0503a448bae462a38
Martin Jakubik <martindzejky@gmail.com>
2023-11-13 17:00:31 +0100

    simple dark theme

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,26 @@
 
 <style scoped>
 main {
+  --background-color: white;
+  --background-color-gradient: #fdfdfd;
+  --text-color: #222;
+
   min-height: 100vh;
   display: grid;
   place-items: center;
   font-family: 'Sriracha', sans-serif;
-  background: radial-gradient(#fff, #fdfdfd);
+  background: var(--background-color)
+    radial-gradient(var(--background-color) 0%, var(--background-color-gradient) 100%);
+  color: var(--text-color);
+
+  @media (prefers-color-scheme: dark) {
+    --background-color: #222;
+    --background-color-gradient: #151515;
+    --text-color: #eee;
+  }
 }
 
 h1 {
   font-size: 5rem;
-  color: #222;
 }
 </style>
