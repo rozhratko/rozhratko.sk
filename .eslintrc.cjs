777ac52a2306e3f5f225217027a224dfa5c4aa06
Martin Jakubik <martindzejky@gmail.com>
2023-11-13 16:49:29 +0100

    update configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+  },
+  plugins: ['simple-import-sort'],
+  rules: {
+    'vue/block-order': ['error', { order: ['script', 'template', 'style'] }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   }
-}
+};
